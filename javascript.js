0e42e3d8dad2b8c4ddbe694a5d9efb83f71396e9
olivier-goy <olivier.goy.37@gmail.com>
2017-03-08 17:21:26 +0100

    Challange of javascripting

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var test = prompt("Hey mon ami ! Tu aimes ça les patates.", "Tape OUI ou NON");
+var test2 = '';
+
+if (test === "oui" ) {
+
+for (var i = 0; i < 7; i++) {
+
+  test2 += "#";
+  console.log(test2);
+  }
+
+}
+
+if (test === "non") {
+  alert("Tu sort");
+}
+
+if (test === null) {
+  alert("Je te sens tiraillé");
+}
